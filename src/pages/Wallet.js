fd2e68488835c2065fde0aee434d00899cb9472f
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-10-31 22:23:58 -0300

    Requisito 2 concluído

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <h1>TrybeWallet</h1>
+        <Header />
+
+      </div>
+    );
   }
 }
 
