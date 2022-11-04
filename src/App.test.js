6c0861f2e8eb1b00044123352211ba438288f539
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-11-04 19:48:44 -0300

    Requisito 6 concluído

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import { renderWithRouterAndRedux } from './tests/helpers/renderWith';
 import App from './App';
 import Wallet from './pages/Wallet';
